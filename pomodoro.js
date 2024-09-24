080fb7736097f4cc3de3bd009d617a0367720c36
hugo theault <hugo.theault@etu.unicen.fr>
2024-09-24 12:50:28 +0200

    commit work/pause

--- pomodoro.js
+++ pomodoro.js
@@ -1,44 +1,67 @@
-const MINUTE = 2;
-const SECOND = 0;
+const MINUTETRAVAIL = 0;
+const SECONDTRAVAIL = 10;
 
-let m = MINUTE+"";
-let s = SECOND+"";
+const MINUTEPAUSE = 0;
+const SECONDPAUSE = 5;
+
+let m = MINUTETRAVAIL;
+let s = SECONDTRAVAIL;
+
+let travail = true;
+
 let Timer = document.getElementById("timer");
-Timer.innerHTML = MINUTE+" : "+SECOND;
+Timer.textContent = caractere(MINUTETRAVAIL)+" : "+caractere(SECONDTRAVAIL);
+
 let timerIntervalle;
+
 let buttonStart = document.getElementById("start");
+let buttonReStart = document.getElementById("reStart");
+
+let bg = document.querySelector("html");
 
 buttonStart.addEventListener('click', ()=>{
     if(timerIntervalle == null){
-        timerIntervalle = setInterval(timer, 10);
+        timerIntervalle = setInterval(timer, 1000);
     }
+    buttonStart.style.display="none";
+    buttonReStart.style.display="inline-block";
 });
+
+buttonReStart.addEventListener('click', ()=>{
+    timerBack();
+});
+
 
 function timer(){
     console.log(m,s)
-    if(s == 0){
-        m -= 1;
-        s = 59;
-        Timer.innerHTML = m+" : "+s;
+    if(s== 0){
+        m-= 1;
+        s= 59;
+        Timer.textContent = caractere(m)+" : "+caractere(s);
     }else {
-        s --;
-        Timer.innerHTML = m+" : "+s;
+        s--;
+        Timer.textContent = caractere(m)+" : "+caractere(s);
     }
-    if(s < 10 ){
-        s = "0"+s;
-        Timer.innerHTML = m+" : "+s;
-    }
-    if(m < 10 && s == 0){
-        m = "0"+m;
-        Timer.innerHTML = m+" : "+s;
-    }
-    if(m == 0 && s == 0){
-        clearInterval(timerIntervalle);
-        timerIntervalle = null;
-        m = MINUTE;
-        s = SECOND+"";
-        Timer.innerHTML = m+" : "+s;
+    if(m== 0 && s== 0 && travail == true){
+        travail = false;
+        m = MINUTEPAUSE;
+        s = SECONDPAUSE;
+        bg.style.backgroundColor="green";
+    }else if(m == 0 && s == 0 && travail == false){
+        travail = true;
+        m = MINUTETRAVAIL;
+        s = SECONDTRAVAIL;
+        bg.style.backgroundColor="red";
     }
 
 }
 
+
+function timerBack(){
+    location.reload();
+}
+
+function caractere(nb)
+{ 
+    return (nb < 10) ? '0'+nb : nb;
+}
